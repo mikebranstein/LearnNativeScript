4c4697d99833d26c6199c7768725a8fcfafea652
Mike Branstein <mike.branstein@gmail.com>
2015-10-10 22:05:03 -0400

    added tns project and ios platform

---
+++ DemoApp/platforms/ios/build/DemoApp.build/Debug-iphonesimulator/DemoApp.build/Objects-normal/i386/main.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/mike/Documents/github/NativeScriptDemoApp/DemoApp/platforms/ios/DemoApp/main.m \
+  /Users/mike/Documents/github/NativeScriptDemoApp/DemoApp/platforms/ios/NativeScript/include/NativeScript.h \
+  /Users/mike/Documents/github/NativeScriptDemoApp/DemoApp/platforms/ios/NativeScript/include/TNSRuntime.h \
+  /Users/mike/Documents/github/NativeScriptDemoApp/DemoApp/platforms/ios/NativeScript/include/TNSRuntime+Inspector.h \
+  /Users/mike/Documents/github/NativeScriptDemoApp/DemoApp/platforms/ios/DemoApp/TNSDebugging.h
