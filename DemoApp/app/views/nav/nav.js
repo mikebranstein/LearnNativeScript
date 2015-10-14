a9167575f1625a1a52e9db5a8976f6b2b89453ec
Mike Branstein <mike.branstein@gmail.com>
2015-10-14 06:25:41 -0400

    add nav to by page name

--- DemoApp/app/views/nav/nav.js
+++ DemoApp/app/views/nav/nav.js
@@ -1,3 +1,13 @@
+var frameModule = require("ui/frame");
+var viewRoot = "views/nav/samples/"
+var topmost;
+
 exports.loaded = function (args) {
+	topmost = frameModule.topmost();
+
 	console.log("Loaded nav page.");
 };
+
+exports.navigateByPageName = function (args) {
+	topmost.navigate(viewRoot + "by-page-name/by-page-name");	
+}
