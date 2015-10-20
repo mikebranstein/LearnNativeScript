17be75139d9235fdf764dc770fa02ae2d8f43ed9
Mike Branstein <mike.branstein@gmail.com>
2015-10-19 21:10:02 -0400

    added goBack() command

--- DemoApp/app/views/nav/samples/by-page-name/by-page-name.js
+++ DemoApp/app/views/nav/samples/by-page-name/by-page-name.js
@@ -1,3 +1,12 @@
+var frameModule = require("ui/frame");
+var topmost;
+
 exports.loaded = function (args) {
+	topmost = frameModule.topmost();
 	console.log("Loaded navigation by page name page.");
-}
+};
+
+exports.navigateBack = function (args) {
+	console.log("Navigating back...");
+	topmost.goBack();
+};
