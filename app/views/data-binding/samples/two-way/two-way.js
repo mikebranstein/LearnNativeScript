a556031165fe914abeda24b6040d715cc00f9087
Mike Branstein <mike.branstein@gmail.com>
2015-11-27 20:55:36 -0500

    add two-way data binding examples

--- app/views/data-binding/samples/two-way/two-way.js
+++ app/views/data-binding/samples/two-way/two-way.js
@@ -1,4 +1,28 @@
+var observableModule = require("data/observable");
+var dialogsModule = require("ui/dialogs");
+var source = new observableModule.Observable({
+	textSource: ""
+});
+var page;
+
 exports.onLoaded = function(args) {
+	page = args.object;
+	
+	var bindingOptions = {
+		sourceProperty: "textSource",
+		targetProperty: "text",
+		twoWay: true
+	};
+	var textField = page.getViewById("text-field");
+	textField.bind(bindingOptions, source);
 	
 	console.log("two-way data binding page loaded");
+};
+
+exports.showDataBoundValue = function(args) {
+	dialogsModule.alert("Data bound value is '" + source.get("textSource") + "'");
+};
+
+exports.resetDataBoundValue = function(args) {
+	source.set("textSource", "");	
 };
